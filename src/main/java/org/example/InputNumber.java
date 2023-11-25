948a02d30cb98a6cb8514c947cb4f56e38650549
Alyona4433 <alena4433@gmail.com>
2023-11-25 21:55:31 +0200

    Added InputNumber Class

---
+++ src/main/java/org/example/InputNumber.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class InputNumber {
+    public static int getNumber() {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = -1;
+
+        while(n <0) {
+            try {
+                System.out.println("Enter any number: ");
+                n = scanner.nextInt();
+            } catch (NumberFormatException e) {
+                System.out.println("Please enter valid input value = ");
+                scanner.nextLine();
+            }
+        } return n;
+    }
+}
---
+++ src/test/java/org/example/InputNumberTest.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import java.io.ByteArrayInputStream;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class InputNumberTest {
+
+    @Test
+    public void testInputNumber() {
+        String input = "5\n";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        int result = InputNumber.getNumber();
+
+        assertEquals(5, result, "Should return inputted value");
+    }
+
+}
